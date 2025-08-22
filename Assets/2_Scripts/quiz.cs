3f05d3cab47a60db398441855013022806d16132
mk <vahyeon2512@sonline20.sen.go.kr>
2025-08-22 11:26:03 +0900

    스프라이트변경

--- Assets/2_Scripts/quiz.cs
+++ Assets/2_Scripts/quiz.cs
@@ -1,20 +1,33 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class quiz : MonoBehaviour
 {
-    [SerializeField]TextMeshProUGUI questionText;
-    [SerializeField]Question_SO question;
-    [SerializeField] TextMeshProUGUI[] answerTextArr = new TextMeshProUGUI[4];
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] Question_SO question;
+    //[SerializeField] TextMeshProUGUI[] answerTextArr = new TextMeshProUGUI[4];
+    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAmswerSprite;
+    [SerializeField] Sprite correctAmswerSprite;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
 
-        for (int i = 0; i < answerTextArr.Length; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            answerTextArr[i].text = question.GetAnswer(i);
+            //answerText[i].text = question.GetAnswer(i);
+            answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetAnswer(i);
         }
     }
 
+    public void OnAnswerButtonClicked(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "정답입니다!";
+            answerButtons[index].GetComponent<Image>().sprite = correctAmswerSprite;
+        }
+    }
 }
